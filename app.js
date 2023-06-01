6e9bd661468c58f207f5ec0da0d9bd343c7394bd
w3i3538 <wei5566@gmail.com>
2023-06-01 19:45:22 +0800

    feat: add mongoose

--- app.js
+++ app.js
@@ -1,6 +1,27 @@
 const express = require('express')
+const mongoose = require('mongoose') //載入 mongoose
+
+// // 加入這段 code, 僅在非正式環境時, 使用 dotenv
+// if (process.env.NODE_ENV !== 'production') {
+//     require('dotenv').config()
+// }
+
 const app = express()
-const port = 3000
+
+// 設定連線到mongoDB
+mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+
+const db = mongoose.connection
+
+// 連線異常
+db.on('error', () => {
+    console.log('mongodb error!')
+})
+
+// 連線成功
+db.once('open', () => {
+    console.log('mongodb connected!')
+})
 
 app.get('/', (req, res) => {
     res.send('hello world!')
@@ -8,5 +29,5 @@ app.get('/', (req, res) => {
 })
 
 app.listen(3000, () => {
-    console.log(`App is running on port ${port}.`)
+    console.log(`App is running on port 3000.`)
 })
