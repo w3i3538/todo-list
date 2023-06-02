386095353adc0ff6ca21f137763f6b6c84f0efc4
w3i3538 <wei5566@gmail.com>
2023-06-02 19:40:09 +0800

    feat: add create todo function

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const mongoose = require('mongoose') //載入 mongoose
 const exphbs = require('express-handlebars')
+const bodyParser = require('body-parser')
 const Todo = require('./models/todo')
 // 加入這段 code, 僅在非正式環境時, 使用 dotenv
 if (process.env.NODE_ENV !== 'production') {
@@ -27,13 +28,33 @@ db.once('open', () => {
 app.engine('hbs', exphbs.engine({ defaultLayout: 'main', extname: '.hbs' }))
 app.set('view engine', 'hbs')
 
+//每一個req都會經過bodyParser使用urlencoded解析
+app.use(bodyParser.urlencoded({ extended:true }))
+
 app.get('/', (req, res) => {
     //拿全部todo資料
     Todo.find()
         .lean()
-        .then(todos => res.render('index',{todos}))
-        .catch(error=> console.log(error))
-    
+        .then(todos => res.render('index', { todos }))
+        .catch(error => console.log(error))
+
+})
+
+app.get('/todos/new', (req, res) => {
+    return res.render('new')
+})
+
+app.post('/todos', (req, res) => {
+    const name = req.body.name
+
+    // const todo = new Todo({ name })
+    // return todo.save()
+    //     .then(() => res.redirect('/'))
+    //     .catch(error => console.log(error))
+    //上下結果相等的(上是先建立todo實體再儲存(可做更多動作但這邊沒這需求)，下是直接建立一筆)
+    return Todo.create({name})
+        .then(() => res.redirect('/'))
+        .catch(error => console.log(error))
 })
 
 app.listen(3000, () => {
