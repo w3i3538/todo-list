e4dafbc2bf4602a544564093c5cd0f38620315e1
w3i3538 <wei5566@gmail.com>
2023-06-12 22:36:21 +0800

    feat: modal todo add isDone params

--- models/todo.js
+++ models/todo.js
@@ -5,6 +5,10 @@ const todoSchema = new Schema({
     name: {
         type: String,
         required: true
+    },
+    isDone: {
+        type: Boolean,
+        default: false  // 預設完成狀態為 false
     }
 })
 
