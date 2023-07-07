d0adcb0897ee2d3b95a351bbb46bab8e88d1787a
w3i3538 <wei5566@gmail.com>
2023-07-07 19:25:34 +0800

    feat: add user modules

--- routes/index.js
+++ routes/index.js
@@ -2,13 +2,15 @@
 const express = require('express')
 const router = express.Router()
 // 引入模組
-// 引入 home 模組程式碼
 const home = require('./modules/home')
 const todos = require('./modules/todos')
+const users = require('./modules/users')
 
-// 將網址結構符合 / 字串的 request 導向 home 模組 
+
+// 將網址結構符合 / 字串的 request 導向 各模組 
 router.use('/', home)
 router.use('/todos', todos)
+router.use('/users', users)
 
 //匯出路由模組
 module.exports = router
