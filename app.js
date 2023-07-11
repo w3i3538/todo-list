d0c548c378b4518663dab87d3ae572961e790718
w3i3538 <wei5566@gmail.com>
2023-07-11 19:36:10 +0800

    feat: add message partials

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const session = require('express-session')
 const exphbs = require('express-handlebars')
 const bodyParser = require('body-parser')
 const methodOverride = require('method-override')
+const flash = require('connect-flash')
 // 加入路由器
 const routes = require('./routes')
 // 載入設定檔，要寫在 express-session 以後
@@ -14,6 +15,7 @@ if (process.env.NODE_ENV !== 'production') {
 
 require('./config/mongoose')
 const app = express()
+const PORT = process.env.PORT || 3000
 
 app.engine('hbs', exphbs.engine({ defaultLayout: 'main', extname: '.hbs' }))
 app.set('view engine', 'hbs')
@@ -29,13 +31,16 @@ app.use(methodOverride('_method'))
 // 呼叫 Passport 函式並傳入 app，這條要寫在路由(routes)之前
 usePassport(app)
 
+app.use(flash())
 app.use((req, res, next) => {
     res.locals.isAuthenticated = req.isAuthenticated()
     res.locals.user = req.user
+    res.locals.success_msg = req.flash('success_msg')
+    res.locals.warning_msg = req.flash('warning_msg')
     next()
 })
 app.use(routes)
 
-app.listen(3000, () => {
-    console.log(`App is running on port 3000.`)
+app.listen(PORT, () => {
+    console.log(`App is running on http://localhost:${PORT}`)
 })
